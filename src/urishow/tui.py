378c8e6e1c4e0f9a5ed4ad3553b1722178d35f84
Borroot <borroot@live.nl>
2020-05-14 23:04:42 +0200

    Add key shortcuts.

--- src/urishow/tui.py
+++ src/urishow/tui.py
@@ -1,11 +1,7 @@
+import functools
+import os
 import curses
 import curses.ascii
-import functools
-
-
-class Event:
-    UP   = -1
-    DOWN =  1
 
 
 class State:
@@ -35,20 +31,24 @@ def _draw(window, state, uris):
     window.refresh()
 
 
-def _handler(window, state, uris, event):
-    if state.current + event >= 0 and state.current + event < len(uris):
-        state.current += event
+def _handle_move(window, state, uris, dy):
+    if state.current + dy >= 0 and state.current + dy < len(uris):
+        state.current += dy
 
-    if   event == Event.UP:
+    if   dy == -1:  # up
         if state.current < state.top:
             window.clear()
             state.top    -= 1
             state.bottom -= 1
-    elif event == Event.DOWN:
+    elif dy ==  1:  # down
         if state.current > state.bottom - 1:
             window.clear()
             state.top    += 1
             state.bottom += 1
+
+
+def _handle_resize(window, state):
+    window.clear()
 
 
 def _receiver(window, state, uris):
@@ -56,11 +56,13 @@ def _receiver(window, state, uris):
         _draw(window, state, uris)
 
         c = window.getch()
-        if   c == ord('k'):
-            _handler(window, state, uris, Event.UP)
-        elif c == ord('j'):
-            _handler(window, state, uris, Event.DOWN)
-        elif c == ord('q'):
+        if   c == curses.KEY_RESIZE:
+            _handle_resize(window, state)
+        elif c == ord('k') or c == curses.KEY_UP:
+            _handle_move(window, state, uris, -1)
+        elif c == ord('j') or c == curses.KEY_DOWN:
+            _handle_move(window, state, uris,  1)
+        elif c == ord('q') or c == 27:  # esc
             return None
         elif c == 10 or c == 13:  # enter
             return uris[state.current]
@@ -70,7 +72,6 @@ def _init(uris, window):
     try:
         curses.use_default_colors()
         curses.curs_set(0)
-        window.notimeout(False)
 
         height, width = window.getmaxyx()
         state = State(width, height, 0, height - 3, 0)
@@ -83,6 +84,7 @@ def show(uris):
     """
     Show all the given urls and return the ones selected by the user.
     """
+    os.environ.setdefault('ESCDELAY', '25')  # no delay when pressing esc
     return curses.wrapper(functools.partial(_init, uris))
 
 print(show(['https://www.{}.example.com'.format(num + 1) for num in range(100)]))
